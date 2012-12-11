ac66a73cd3bf245628914103b97e4ae637aca66c
Alexander Savinoff <hinstance@yandex.ru>
2012-12-11 19:46:41 +0400

    Standard log tag added

--- CustomLogEntity.pas
+++ CustomLogEntity.pas
@@ -7,6 +7,12 @@ type
   public
     procedure Write(const aText: string); overload; virtual; abstract;
     procedure Write(const aTag, aText: string); overload; virtual; abstract;
+  public type
+    StandardTag = object
+    public const
+      Error = 'ERROR';
+      Warning = 'WARNING';
+    end;
   end;
 
 implementation
